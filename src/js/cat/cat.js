5fc0b3524afb6495d65fbc58b69c3c001c701b8b
WangXiongShi <1015580705@qq.com>
2017-10-24 17:20:35 +0800

    第一次上传

---
+++ src/js/cat/cat.js
@@ -0,0 +1,19 @@
+// sadasdasdasd
+// asdasd
+// sadasdas
+// asdasdasdas
+// qweqwe
+function a(){
+    console.log('sssss');
+    var m = 100;
+    var n = 200;
+    var s = m + n;
+    return {
+        m:m,
+        n:n,
+        s:s
+    }
+}
+var obj = a();
+console.log(obj);
+
